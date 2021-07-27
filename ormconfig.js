0cabcf0cc87a0715d0162a63166f2d81aac2cb78
Alexandre Magno AM. de Carvalho <alexandremagno@p4pro.com.br>
2021-07-27 11:46:33 -0300

    Configuração do ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,10 @@
 module.exports = {
     "type": "postgres",
-    "url": process.env.DATABASE_URL,
+    "host": process.env.DATABASE_HOST,
+    "port": 5432,
+    "username": process.env.DATABASE_USERNAME,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_DB,
     "synchronize": true,
     "logging": true,
     "ssl": true,
